833447e4bccd9df4ccc8cdbfd123913a87c625b3
Roger Miller <rogeralexmiller@gmail.com>
2016-06-25 13:53:45 -0400

    map out castling

--- pieces/king.rb
+++ pieces/king.rb
@@ -8,8 +8,22 @@ class King < SteppingPiece
     @moved = false
   end
 
+  # castling:
+  # Castling may only be done if the king has never moved, the rook involved has
+  # never moved, the squares between the king and the rook involved are unoccupied,
+  # the king is not in check, and the king does not cross over or end on a square
+  # in which it would be in check.
+
+  # 1. check if king has ever moved. store as a boolean. done
+  # 2. check if castles have ever moved. store as a boolean. done
+  # 3. check if squares between king and rook are unoccupied. 3 on queen side. 2 on kingside
+  # 4. check if any of those squares are in check.
+  # 5. If all of these conditions are met, show the castle as a potential_move.
+
+
   def can_castle?
-    
+    return false if moved
+    return false if castle_moved
   end
 
   def moves
